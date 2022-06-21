19ff90c28cc5810b87b08e11f125faee5c2acd74
Grochowsky <grzesiugg@gmail.com>
2022-06-21 19:57:16 +0200

    finish-end

--- app.js
+++ app.js
@@ -163,7 +163,6 @@ async function loadPassword() {
             })
            
         })
-        //TU BEDZIE KOT Z ZEWNATRZ KONIEC
         
     })
    
@@ -172,7 +171,6 @@ async function loadPassword() {
 
 loadPassword()
 
-/*lalsdalsd*/ 
 
 
 
@@ -186,75 +184,3 @@ loadPassword()
 
 
 
-
-
-
-
-// const container = document.querySelector('.container');
-// const API_URL = 'https://inwestowaniepro.pl/wp-json/wp/v2/posts';
-
-
-
-
-// const articles = () => {
-
-//     const newArticle = document.createElement('div');
-
-//     fetch(API_URL)
-//     .then(response => response.json())
-//     .then(data => { 
-//         data.forEach(element => {
-//             console.log(element)
-//             const siusiak = document.createElement('div');
-//             siusiak.innerHTML =`<h1> ${element.title.rendered}</h1>`
-//             container.appendChild(siusiak)
-//         });
-//     }
-        
-//         )
-
-        
-// }
-
-// let d = new Date();
-
-
-// console.log(Date());
-
-// let x = document.createElement('p');
-// x.innerHTML = d;
-// document.body.appendChild(x)
-
-
-/* const container = document.querySelector('.container');
-const API_URL = 'https://inwestowaniepro.pl/wp-json/wp/v2/posts';
-
-
-
-
-
-const articles = () => {
-
-    const newArticle = document.createElement('div');
-
-    fetch(API_URL)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data[0]);
-        newArticle.innerHTML = `
-        <div class="article1">
-            <img src="${data[0].yoast_head_json.og_image[0].url}" alt="">
-
-            <h1>${data[0].title.rendered}</h1>
-        </div>
-        `
-        container.appendChild(newArticle)
-
-    });
-
-
-    
-  
-}
-
-articles()*/
