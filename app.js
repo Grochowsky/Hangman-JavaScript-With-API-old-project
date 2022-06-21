f0dd8ae0a87eb5c350195db8d85224e0036a4fbf
Grochowsky <grzesiugg@gmail.com>
2022-06-21 19:43:00 +0200

    finish2

--- app.js
+++ app.js
@@ -1,62 +1,58 @@
-const p = document.querySelectorAll('p');
-const haslo = "siemanko".toUpperCase();
-let ukryte;
-let jeden;
-const hasloContainer = document.querySelector('.haslo1');
-let newOne = [];
-ukryte = haslo.split('')
-
-const podest = document.querySelector('.podest');
-const pion = document.querySelector('.pion');
-const poziom = document.querySelector('.poziom');
-const lina = document.querySelector('.lina');
-const glowa = document.querySelector('.glowa');
-const tulow = document.querySelector('.tulow');
-const preka = document.querySelector('.preka');
-const lreka = document.querySelector('.lreka');
-const pnoga = document.querySelector('.pnoga');
-const lngoa = document.querySelector('.lnoga');
+const APIurl = 'https://random-word-api.herokuapp.com/word';
 
 
 
-function invisiblePassword(){
-    for(let x = 0; x<ukryte.length; x++){
-        newOne[x] = " _ ";
 
-    }
-    console.log(newOne)
-    hasloContainer.innerHTML = newOne.join('');
-    
-}
-invisiblePassword()
+let password;
+const container = document.querySelector('.container')
 
 
-console.log(ukryte)
+// const test1 = () => {
+//     fetch(APIurl)
+//   .then(response => response.json())
+//   .then(data => {
+//     password = data;
+//     console.log(password);
+//   });
+// }
 
-let badClickCount = 0;
+async function loadPassword() {
 
-
-p.forEach( (element) => {
-    element.addEventListener('click', () => {
+    const response = await fetch(APIurl);
+    fetch(APIurl)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data[0])
+        // TTU BEDZIE KODD Z ZEWNATRZ //
+        const p = document.querySelectorAll('p');
+        const haslo = data[0].toUpperCase();
+        let ukryte;
+        const hasloContainer = document.querySelector('.haslo1');
+        let newOne = [];
+        ukryte = haslo.split('')
         
-        let checkTheLetter = false; // sprawdzanie litery czy wystepuje wewnatrz hasla / domyslnie na false/ jesli jest wtedy zmienimy na true i nic nie zrobimy, a jesli zostanie fals wtedy zostanie wykonana nastepna czesc kodu
-        for(let i = 0; i<= haslo.length; i++){
-            if(element.innerHTML == haslo[i] ){ //sprawdz czy dana litera znajduje sie w hasle
-            newOne[i] = element.innerHTML; // jeśli tak to przypis do indexu elementu newone tą wlasnie litere
-            hasloContainer.innerHTML = newOne.join(''); // nastepnie dodaj nowe haslo wewnątrz html haslocontainer
-            element.style.backgroundColor = 'green';
-            checkTheLetter = True;
-         
+        const podest = document.querySelector('.podest');
+        const pion = document.querySelector('.pion');
+        const poziom = document.querySelector('.poziom');
+        const lina = document.querySelector('.lina');
+        const glowa = document.querySelector('.glowa');
+        const tulow = document.querySelector('.tulow');
+        const preka = document.querySelector('.preka');
+        const lreka = document.querySelector('.lreka');
+        const pnoga = document.querySelector('.pnoga');
+        const lngoa = document.querySelector('.lnoga');
+        
+        
+        
+        
+        
+        function invisiblePassword(){
+            for(let x = 0; x<ukryte.length; x++){
+                newOne[x] = " _ ";
+        
             }
-        }
-        if (checkTheLetter == false) {
-            if(element.style.backgroundColor === "red"){
-                console.log(element) // sprawdzdamy czy element ktory kliknelismy, czy zostal juz klikniety czy nie aby nie naliczac  bacclickcount....
-            } else {
-                element.style.backgroundColor = "red";
-                badClickCount++
-                console.log(`${badClickCount}`)
-            }
+            console.log(newOne)
+            hasloContainer.innerHTML = newOne.join('');
             
         }
         invisiblePassword()
@@ -171,9 +167,14 @@ p.forEach( (element) => {
         
     })
    
-})
+}
 
-console.log("siemank")
+
+loadPassword()
+
+/*lalsdalsd*/ 
+
+
 
 
 
